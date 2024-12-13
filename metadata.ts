4b1ba78c452ab39bd2337deb6887f4d74adbba25
ZireaelGit <nelo.labhart@gmail.com>
2024-12-13 18:00:13 +0900

    init

---
+++ metadata.ts
@@ -0,0 +1,13 @@
+const metadata = {
+    "name": "Zireael",
+    "symbol": "Zireael",
+    "description": "Zireael token",
+    "image": "./upload/img.png",
+    "showName": true,
+    "createdOn": "https://pump.fun",
+    "twitter": "https://x.com/Zireael",
+    "telegram": "https://t.me/Zireael",
+    "website": "https://Zireael"
+}
+
+export default metadata;
